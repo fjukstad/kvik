a078ed5e63c1a040b74d11a33effccef65ee0d53
Bjørn Fjukstad <bfj001@post.uit.no>
2013-09-10 16:09:07 +0200

    get dir from url working

--- gocache.go
+++ gocache.go
@@ -2,8 +2,23 @@ package main
 
 import(
     "fmt"
+    "net/http"
 )
 
 func main(){
     fmt.Println("hello"); 
+}
+
+func Get(url string) (resp *Response, err error) {
+    
+
+    
+
+}
+
+func getDirectory(url string) (dir string) {
+	urlTokens := strings.Split(url, "/")
+	strippedUrl := urlTokens[3:]
+	dir = strings.Join(strippedUrl,"/")
+	return 
 }
