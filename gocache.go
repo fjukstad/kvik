0d5c1485f68b2dc7f9483613e74e7cc9a89e7c1d
Bjørn Fjukstad <bfj001@post.uit.no>
2013-09-10 17:49:52 +0200

    it's coming along now. Don't know if we can write the struct directly to disk using json stuff...

--- gocache.go
+++ gocache.go
@@ -3,31 +3,116 @@ package main
 import(
     "fmt"
     "net/http"
+    "errors"
+    "os"
+    "strings"
+    "log"
+    "encoding/json" 
 )
 
 func main(){
-    fmt.Println("hello"); 
+    fmt.Println("hello");
+    url := "http://blog.golang.org/error-handling-and-go"
+    Get(url)
 }
 
-func Get(url string) (resp *Response, err error) {
+func Get(url string) (resp *http.Response, err error) {
     
-    resp, err := getFromCache(url) 
+    resp, err = getFromCache(url) 
     
     if err != nil{
-        return http.Get(url) 
+        log.Println(url,"could not be served out of cache.", err)
+        return getFromWeb(url) 
     }
 
     return   
 }
 
-func getFromCache(url string) (resp *Response, err error) {
+// Fetch contents of url from web and write to cache
+func getFromWeb(url string) (resp *http.Response, err error){
+    resp, err = http.Get(url) 
     
+    if err != nil{
+        return
+    }
+    writeToCache(url, resp)   
+    return 
+}
 
+func writeToCache(url string, resp *http.Response){
     
+    log.Println("Writing response to cache")
+
+    b, err := json.Marshal(resp)
+
+    if err != nil {
+        log.Println(err)
+    }
+    
+    filename := getFilePath(url)
+    file, err := os.Create(filename)
+    
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    // Close and check for error on exit 
+    defer func() {
+        if err := file.Close(); err != nil {
+            log.Println(err)
+        }
+    }()
+        
+    _, err = file.Write(b)
+    if err != nil{
+        log.Println(err)
+    }
+    
+    log.Println("Successfully written to cache"); 
+}
+
+// Try to fetch contents of url from cache
+func getFromCache(url string) (resp *http.Response, err error) {
+    
+    filename := getFilePath(url)
+    
+    file, err := os.Open(filename) 
+    if err != nil{
+        err = errors.New("File '"+filename+"' not Found") 
+        return 
+    }
+    
+    resp = readFromFile(file)
+
+    fmt.Println(file)
+    return
+
 } 
 
+// Read cache entry from file
+func readFromFile(file *os.File) (resp *http.Response) {
+    
+    buf := make([]byte, 1024)
+    
+    for {
+        _, err := file.Read(buf)
+        if err != nil{
+            break
+        }
+    }
 
-func getDirectory(url string) (dir string) {
+    err := json.Unmarshal(buf,resp)
+
+    if err != nil {
+        log.Print(err) 
+    }
+
+
+    return
+}
+
+func getFilePath(url string) (dir string) {
 	urlTokens := strings.Split(url, "/")
     // 3 because we need to strip away 'http:', ' ', and hostname
     strippedUrl := urlTokens[3:] 
