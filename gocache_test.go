d034f1a645ce2fd75d3aa3099f3a3c20daf68007
Bjørn Fjukstad <bfj001@post.uit.no>
2013-09-20 09:22:42 +0200

    trying out this testing package. tests aren't currently any good, but will hopefully be one day

---
+++ gocache_test.go
@@ -0,0 +1,44 @@
+package gocache
+
+import (
+    "testing"
+    "os"
+    "log"
+) 
+
+// 
+func Test_Get_1(t *testing.T) {
+    clearCache()
+    
+    t.Log("On first request, url should not be found in cache")
+    url := "http://blog.golang.org/error-handling-and-go"
+    
+    _, err := Get(url) 
+    
+    if err != nil {
+        t.Error("Get failed: ", err)
+        t.FailNow()
+    }
+    
+}
+
+func Test_Get_2(t *testing.T) {
+
+    badurl := "htt://"
+
+    _, err := Get(badurl)
+    
+    if err != nil {
+        t.Log("Bad url got error:" , err)
+        return
+    }
+
+    t.Error("Request with bad url succeeded") 
+
+}
+
+
+func clearCache() {
+    log.Print("Clearing cache")
+    os.RemoveAll("cache") 
+}
