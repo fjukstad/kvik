dbe86cc94223616a1ecd6f5bc23e05507c21e7fc
Bjørn Fjukstad <bfj001@post.uit.no>
2013-10-23 14:59:20 +0200

    backend supports shipping visualizations to the client. they should maybe be stored as files but now they are just stored as strings

--- backend.go
+++ backend.go
@@ -41,9 +41,23 @@ type NOWACService struct {
     getInfo gorest.EndPoint `method:"GET"
                             path:"/info/{Items:string}/{InfoType:string}"
                             output:"string"`
-                            
+
+    getGeneVis gorest.EndPoint `method:"GET"
+                            path:"/vis/{Gene:string}"
+                            output:"string"`
+                      
 
 }
+
+func (serv NOWACService) GetGeneVis(Gene string) string {
+    addAccessControlAllowOriginHeader(serv)     
+    
+    log.Print("Returning the VIS code for gene: ", Gene)
+    code := GeneVisCode(Gene)
+    log.Print("Returning:", code) 
+    return code
+}
+
 
 func (serv NOWACService) GetInfo(Items string, InfoType string) string {
 
@@ -103,4 +117,96 @@ func parsePathwayInput(input string) ([] string) {
     
     return b
 
+}
+
+
+func GeneVisCode(gene string) string {
+    vis := `<style>
+
+
+    body {
+      font: 10px sans-serif;
+    }
+
+    .bar rect {
+      fill: steelblue;
+      shape-rendering: crispEdges;
+    }
+
+    .bar text {
+      fill: #fff;
+    }
+
+    .axis path, .axis line {
+      fill: none;
+      stroke: #000;
+      shape-rendering: crispEdges;
+    }
+
+    </style>
+    <body>
+    <script src="http://d3js.org/d3.v2.min.js?2.10.0"></script>
+    <script>
+
+    // Generate an Irwin–Hall distribution of 10 random variables.
+    var values = d3.range(1000).map(d3.random.irwinHall(10));
+
+    // A formatter for counts.
+    var formatCount = d3.format(",.0f");
+
+    var margin = {top: 10, right: 30, bottom: 30, left: 30},
+        width = 500 - margin.left - margin.right,
+        height = 250 - margin.top - margin.bottom;
+
+    var x = d3.scale.linear()
+        .domain([0, 1])
+        .range([0, width]);
+
+    // Generate a histogram using twenty uniformly-spaced bins.
+    var data = d3.layout.histogram()
+        .bins(x.ticks(20))
+        (values);
+
+    var y = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) { return d.y; })])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var svg = d3.select(".visman").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var bar = svg.selectAll(".bar")
+        .data(data)
+      .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+    bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(data[0].dx) - 1)
+        .attr("height", function(d) { return height - y(d.y); });
+
+    bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", 6)
+        .attr("x", x(data[0].dx) / 2)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatCount(d.y); });
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    </script>
+    `
+
+    return vis
+    
 }
