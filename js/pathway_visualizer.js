c7fd5dfb821dcf058b128f2836aa3ad277b6500c
Bjørn Fjukstad <bfj001@post.uit.no>
2014-02-27 10:10:40 +0100

    fixed bug where js loaded graph two times.

--- js/pathway_visualizer.js
+++ js/pathway_visualizer.js
@@ -35,7 +35,7 @@ function getVisServerAddress() {
 
 
 
-$(loadCy = function(){
+loadCy = function(){
     options = {
         layout: {
             name: 'preset', 
@@ -160,6 +160,7 @@ $(loadCy = function(){
 
 
             // Load data from JSON 
+            console.log(wsURL)
             var socket = new WebSocket(wsURL); 
             socket.onmessage = function(m){
                 var message = JSON.parse(m.data); 
@@ -194,8 +195,30 @@ $(loadCy = function(){
 
 
     $('#cy').cytoscape(options); 
+/*
+    $('#cy').cytoscapePanzoom({
+        zoomFactor: 0.05, // zoom factor per zoom tick
+        zoomDelay: 45, // how many ms between zoom ticks
+        minZoom: 0.1, // min zoom level
+        maxZoom: 10, // max zoom level
+        fitPadding: 50, // padding when fitting
+        panSpeed: 10, // how many ms in between pan ticks
+        panDistance: 10, // max pan distance per tick
+        panDragAreaSize: 75, // the length of the pan drag box in which the vector for panning is calculated (bigger = finer control of pan speed and direction)
+        panMinPercentSpeed: 0.25, // the slowest speed we can pan by (as a percent of panSpeed)
+        panInactiveArea: 8, // radius of inactive area in pan drag box
+        panIndicatorMinOpacity: 0.5, // min opacity of pan indicator (the draggable nib); scales from this to 1.0
+        autodisableForMobile: true, // disable the panzoom completely for mobile (since we don't really need it with gestures like pinch to zoom)
 
-});
+        // icon class names
+        sliderHandleIcon: 'fa fa-minus',
+        zoomInIcon: 'fa fa-plus',
+        zoomOutIcon: 'fa fa-minus',
+        resetIcon: 'fa fa-expand'
+    });
+    */
+    
+}
 
 
 function GenerateInfoPanel(info){
@@ -297,4 +320,6 @@ function addCSS(cssPath) {
 addCSS("/css/pathway-visualizer.css"); 
 
 
-
+window.onerror = function(error) {
+    alert(error);
+};
