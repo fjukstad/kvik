6ecbaff3286f1fb1501405eda5c5ebd003eb227f
Bjørn Fjukstad <bfj001@post.uit.no>
2014-04-28 17:49:37 +0200

    gofmt

--- gene.go
+++ gene.go
@@ -1,296 +1,290 @@
 package kegg
 
 import (
-    "log"
-    "github.com/fjukstad/gocache"
-    "io"
-    "encoding/csv"
-    "strings"
-    "fmt"
-    "strconv"
-    "encoding/json"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"github.com/fjukstad/gocache"
+	"io"
+	"log"
+	"strconv"
+	"strings"
 )
 
 type Gene struct {
-    Id string                   
-    Name string                 
-    Definition string           
-    Orthology string            
-    Organism string             
-    Pathways []string
-    Modules []string 
-    Diseases []string
-    Drug_Target [] string
-    Classes []string
-    Position string
-    Motif string
-    DBLinks map[string]string
-    Structure string
-    AASEQ Sequence              
-    NTSEQ Sequence
+	Id          string
+	Name        string
+	Definition  string
+	Orthology   string
+	Organism    string
+	Pathways    []string
+	Modules     []string
+	Diseases    []string
+	Drug_Target []string
+	Classes     []string
+	Position    string
+	Motif       string
+	DBLinks     map[string]string
+	Structure   string
+	AASEQ       Sequence
+	NTSEQ       Sequence
 }
 
 type Sequence struct {
-    length int
-    Sequence string
+	length   int
+	Sequence string
 }
 
 func GetGene(id string) Gene {
-    baseURL := "http://rest.kegg.jp/get/hsa:"
-    url := baseURL + id
-    
-    response, err := gocache.Get(url)
-    if err != nil{
-        log.Panic("Cannot download from url:",err)
-    }
-    
-    /*
-    body, err := ioutil.ReadAll(response.Body)
-    if err != nil{
-        log.Panic("Error reading body:",err)
-    }
-    */
+	baseURL := "http://rest.kegg.jp/get/hsa:"
+	url := baseURL + id
 
-    gene := parseGeneResponse(response.Body) 
+	response, err := gocache.Get(url)
+	if err != nil {
+		log.Panic("Cannot download from url:", err)
+	}
 
-    return gene
+	/*
+	   body, err := ioutil.ReadAll(response.Body)
+	   if err != nil{
+	       log.Panic("Error reading body:",err)
+	   }
+	*/
+
+	gene := parseGeneResponse(response.Body)
+
+	return gene
 }
 
 type GenePathways struct {
-    GeneId string
-    Pathways []string
+	GeneId   string
+	Pathways []string
 }
 
 // Fetch pathways for a specific gene
 func Pathways(g Gene) GenePathways {
- 
-    pws := GenePathways{g.Id, g.Pathways} 
-    return pws 
+	pws := GenePathways{g.Id, g.Pathways}
+	return pws
 
 }
 
-
-// TODO: 
+// TODO:
 
 func PathwaysJSON(pws GenePathways) string {
-    b, err := json.Marshal(pws) 
-    if err != nil {
-        log.Panic("marshaling went bad: ", err)
-    }
-    return string(b)
+	b, err := json.Marshal(pws)
+	if err != nil {
+		log.Panic("marshaling went bad: ", err)
+	}
+	return string(b)
 }
 
+func GeneJSON(g Gene) string {
 
-func GeneJSON(g Gene) string{
+	b, err := json.Marshal(g)
+	if err != nil {
+		log.Panic("Marshaling went bad: ", err)
+	}
 
-    b, err := json.Marshal(g)
-    if err != nil {
-        log.Panic("Marshaling went bad: ", err)
-    }
-
-    return string(b)
+	return string(b)
 
 }
 
 func (g Gene) Print() {
-    fmt.Println("\nGene") 
-    fmt.Println("\tId:", g.Id)
-    fmt.Println("\tName:", g.Name)
-    fmt.Println("\tDefinition:", g.Definition) 
-    fmt.Println("\tOrthology:", g.Orthology)
-    fmt.Println("\tOrganism:", g.Organism)
-    fmt.Println("\tPathways:", g.Pathways)
-    fmt.Println("\tClasses:", g.Classes)
-    fmt.Println("\tPosition:", g.Position)
-    fmt.Println("\tMotif:", g.Motif)
-    fmt.Println("\tDBLinks:", g.DBLinks)
-    fmt.Println("\tStructure:", g.Structure)
-    //fmt.Println("\tAASEQ:", g.AASEQ)
-    //fmt.Println("\tNTSEQ:", g.NTSEQ)
-    fmt.Println("")
+	fmt.Println("\nGene")
+	fmt.Println("\tId:", g.Id)
+	fmt.Println("\tName:", g.Name)
+	fmt.Println("\tDefinition:", g.Definition)
+	fmt.Println("\tOrthology:", g.Orthology)
+	fmt.Println("\tOrganism:", g.Organism)
+	fmt.Println("\tPathways:", g.Pathways)
+	fmt.Println("\tClasses:", g.Classes)
+	fmt.Println("\tPosition:", g.Position)
+	fmt.Println("\tMotif:", g.Motif)
+	fmt.Println("\tDBLinks:", g.DBLinks)
+	fmt.Println("\tStructure:", g.Structure)
+	//fmt.Println("\tAASEQ:", g.AASEQ)
+	//fmt.Println("\tNTSEQ:", g.NTSEQ)
+	fmt.Println("")
 }
 
 func parseGeneResponse(response io.ReadCloser) Gene {
-    
-    tsv := csv.NewReader(response) 
-    tsv.Comma = '\t'
-    tsv.Comment = '#'
-    tsv.LazyQuotes = true
-    tsv.TrailingComma = true
-    tsv.TrimLeadingSpace = false
-    
 
-    records, err := tsv.ReadAll()
-    
-    if err != nil {
-        log.Panic("Error reading records:", err)
-    }
-    gene := Gene{}
+	tsv := csv.NewReader(response)
+	tsv.Comma = '\t'
+	tsv.Comment = '#'
+	tsv.LazyQuotes = true
+	tsv.TrailingComma = true
+	tsv.TrimLeadingSpace = false
 
-    tmp := make([]string, 0)
-    current := ""
-    sequence := "" 
-    for i := range records {
-        
-        line := strings.Split(records[i][0]," ")
+	records, err := tsv.ReadAll()
 
-        switch line[0] {
-        case "ENTRY":
-            // parsing to extract id
-            a := strings.Join(line[7:], " ")
-            b :=  strings.Split(a, " ")[0]
-            gene.Id = b
+	if err != nil {
+		log.Panic("Error reading records:", err)
+	}
+	gene := Gene{}
 
-        case "NAME":
-            gene.Name = strings.Join(line[8:], " ") 
+	tmp := make([]string, 0)
+	current := ""
+	sequence := ""
+	for i := range records {
 
-        case "DEFINITION":
-            gene.Definition = strings.Join(line[2:]," ") 
-        
-        case "ORTHOLOGY":
-            gene.Orthology = strings.Join(line[3:], " ") 
-        
-        case "ORGANISM": 
-            gene.Organism = strings.Join(line[4:], " ")
-        
-        case "PATHWAY": 
-            current = "PATHWAY"
-            // Parsing to extract the hsa12345 string
-            a := strings.Join(line[5:], " ")
-            b :=  strings.Split(a, " ")[0]
-            tmp = append(tmp, b)
+		line := strings.Split(records[i][0], " ")
 
-        case "DISEASE":
+		switch line[0] {
+		case "ENTRY":
+			// parsing to extract id
+			a := strings.Join(line[7:], " ")
+			b := strings.Split(a, " ")[0]
+			gene.Id = b
 
-            if(current == "PATHWAY"){
-                gene.Pathways = tmp
-            }
-            if(current == "MODULE"){
-                gene.Modules = tmp
-            }
-            current = "DISEASE"
-            tmp = make([]string, 0)
-            a := strings.Join(line[5:], " ")
-            tmp = append(tmp, a)
+		case "NAME":
+			gene.Name = strings.Join(line[8:], " ")
 
-        case "MODULE":
-           if(current == "PATHWAY"){
-                gene.Pathways = tmp
-            }
-            if(current == "DISEASE"){
-                gene.Diseases = tmp
-            }
+		case "DEFINITION":
+			gene.Definition = strings.Join(line[2:], " ")
 
-            current = "MODULE"
-            tmp = make([]string, 0)
-            a := strings.Join(line[6:], " ")
-            tmp = append(tmp, a)
+		case "ORTHOLOGY":
+			gene.Orthology = strings.Join(line[3:], " ")
 
-        case "DRUG_TARGET":
-            if(current == "DISEASE"){ 
-                gene.Diseases = tmp
-            } 
-            current = "DRUG_TARGET"
-            tmp = make([]string, 0)
-            a := strings.Join(line[1:], " ")
-            tmp = append(tmp,a)
+		case "ORGANISM":
+			gene.Organism = strings.Join(line[4:], " ")
 
-        case "CLASS":
-            if(current == "PATHWAY"){
-                gene.Pathways = tmp
-            }
-            if(current == "DISEASE"){
-                gene.Diseases = tmp
-            }
-            
-            if(current == "MODULE"){
-                gene.Modules = tmp
-            }
-            if(current == "DRUG_TARGET"){
-                gene.Drug_Target = tmp
-            }
+		case "PATHWAY":
+			current = "PATHWAY"
+			// Parsing to extract the hsa12345 string
+			a := strings.Join(line[5:], " ")
+			b := strings.Split(a, " ")[0]
+			tmp = append(tmp, b)
 
+		case "DISEASE":
 
-            current = "CLASS"
-            tmp = make([]string, 0)
-            a := strings.Join(line[7:], " ")
-            tmp = append(tmp, a)
-        
-        case "POSITION":
-            current = "POSITION"
-            gene.Classes = tmp
-            gene.Position = strings.Join(line[4:], " ")
+			if current == "PATHWAY" {
+				gene.Pathways = tmp
+			}
+			if current == "MODULE" {
+				gene.Modules = tmp
+			}
+			current = "DISEASE"
+			tmp = make([]string, 0)
+			a := strings.Join(line[5:], " ")
+			tmp = append(tmp, a)
 
-        case "MOTIF":
-            gene.Motif = strings.Join(line[7:], " ")
+		case "MODULE":
+			if current == "PATHWAY" {
+				gene.Pathways = tmp
+			}
+			if current == "DISEASE" {
+				gene.Diseases = tmp
+			}
 
-        case "DBLINKS": 
-            current = "DBLINKS"
-            tmp = make([] string, 0) 
-            a := strings.Join(line[5:], " ")
-            tmp = append(tmp, a) 
-            e := strings.Split(a, ":")
-            gene.DBLinks = make(map[string]string)
-            gene.DBLinks[e[0]] = e[1]
+			current = "MODULE"
+			tmp = make([]string, 0)
+			a := strings.Join(line[6:], " ")
+			tmp = append(tmp, a)
 
-        case "STRUCTURE": 
-            current = "STRUCTURE"
-            gene.Structure = strings.Join(line[3:], " ")
+		case "DRUG_TARGET":
+			if current == "DISEASE" {
+				gene.Diseases = tmp
+			}
+			current = "DRUG_TARGET"
+			tmp = make([]string, 0)
+			a := strings.Join(line[1:], " ")
+			tmp = append(tmp, a)
 
-        case "AASEQ":
-            current = "AASEQ"
-            a := strings.Join(line[7:], " ")
-            length, err := strconv.Atoi(a)
-            if err != nil{
-                log.Panic("AASEQ PARSING ERROR:", err);
-            }
-            gene.AASEQ = Sequence{length, ""}
-        
-        case "NTSEQ":
-            gene.AASEQ.Sequence = sequence
-            current = "NTSEQ"
-            sequence =""
-            a := strings.Join(line[7:], " ")
-            length, err := strconv.Atoi(a)
-            if err != nil{
-                log.Panic("NTSEQ PARSING ERROR:", err);
-            }
-            gene.NTSEQ = Sequence{length, ""}
+		case "CLASS":
+			if current == "PATHWAY" {
+				gene.Pathways = tmp
+			}
+			if current == "DISEASE" {
+				gene.Diseases = tmp
+			}
 
-        case "///":
-            gene.NTSEQ.Sequence = sequence
+			if current == "MODULE" {
+				gene.Modules = tmp
+			}
+			if current == "DRUG_TARGET" {
+				gene.Drug_Target = tmp
+			}
 
-        default:
-            if(current == "PATHWAY"){
-                // Again some fancy parsing to extract hsa1234 string
-                a := strings.Join(line[12:], " ")
-                b :=  strings.Split(a, " ")[0]
-                tmp = append(tmp, b)
-            }
-            if(current == "CLASS" ||
-                current == "MODULE" ||
-                current == "DRUG_TARGET"){
+			current = "CLASS"
+			tmp = make([]string, 0)
+			a := strings.Join(line[7:], " ")
+			tmp = append(tmp, a)
 
-                // Parsing, not very pretty...
-                a := strings.Join(line[0:], " ")
-                b := strings.Replace(a, "    ", "",-1)
-                tmp = append(tmp, b)
-            }
-            if(current == "DBLINKS"){
-                a := strings.Join(line[0:], " ")
-                b := strings.Replace(a, "    ", "",-1)
-                e := strings.Split(b, ":")
-                gene.DBLinks[e[0]] = e[1]
-            }
-            if(current == "AASEQ" ||
-                current == "NTSEQ" ){
-                a := strings.Join(line[0:], " ")
-                b := strings.Replace(a, "    ", "",-1)
-                sequence += b
-            }
-        }
-    }
+		case "POSITION":
+			current = "POSITION"
+			gene.Classes = tmp
+			gene.Position = strings.Join(line[4:], " ")
 
-    return gene
+		case "MOTIF":
+			gene.Motif = strings.Join(line[7:], " ")
+
+		case "DBLINKS":
+			current = "DBLINKS"
+			tmp = make([]string, 0)
+			a := strings.Join(line[5:], " ")
+			tmp = append(tmp, a)
+			e := strings.Split(a, ":")
+			gene.DBLinks = make(map[string]string)
+			gene.DBLinks[e[0]] = e[1]
+
+		case "STRUCTURE":
+			current = "STRUCTURE"
+			gene.Structure = strings.Join(line[3:], " ")
+
+		case "AASEQ":
+			current = "AASEQ"
+			a := strings.Join(line[7:], " ")
+			length, err := strconv.Atoi(a)
+			if err != nil {
+				log.Panic("AASEQ PARSING ERROR:", err)
+			}
+			gene.AASEQ = Sequence{length, ""}
+
+		case "NTSEQ":
+			gene.AASEQ.Sequence = sequence
+			current = "NTSEQ"
+			sequence = ""
+			a := strings.Join(line[7:], " ")
+			length, err := strconv.Atoi(a)
+			if err != nil {
+				log.Panic("NTSEQ PARSING ERROR:", err)
+			}
+			gene.NTSEQ = Sequence{length, ""}
+
+		case "///":
+			gene.NTSEQ.Sequence = sequence
+
+		default:
+			if current == "PATHWAY" {
+				// Again some fancy parsing to extract hsa1234 string
+				a := strings.Join(line[12:], " ")
+				b := strings.Split(a, " ")[0]
+				tmp = append(tmp, b)
+			}
+			if current == "CLASS" ||
+				current == "MODULE" ||
+				current == "DRUG_TARGET" {
+
+				// Parsing, not very pretty...
+				a := strings.Join(line[0:], " ")
+				b := strings.Replace(a, "    ", "", -1)
+				tmp = append(tmp, b)
+			}
+			if current == "DBLINKS" {
+				a := strings.Join(line[0:], " ")
+				b := strings.Replace(a, "    ", "", -1)
+				e := strings.Split(b, ":")
+				gene.DBLinks[e[0]] = e[1]
+			}
+			if current == "AASEQ" ||
+				current == "NTSEQ" {
+				a := strings.Join(line[0:], " ")
+				b := strings.Replace(a, "    ", "", -1)
+				sequence += b
+			}
+		}
+	}
+
+	return gene
 }
-
