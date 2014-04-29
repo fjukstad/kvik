000dae7e1b8cca52210f20e795ec60c0a7feea07
Bjørn Fjukstad <bfj001@post.uit.no>
2014-04-29 17:25:12 +0200

    Added support for downloading images
    Tool now downloads the pathway image as well. as requested by kenneth.

--- pathway-info/pathway-info.go
+++ pathway-info/pathway-info.go
@@ -1,77 +1,102 @@
 package main
 
 import (
-    "nowac/kegg"
-    "fmt"
-    "encoding/json"
-    "flag"
-    "os"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"nowac/kegg"
+	"os"
 )
 
-type Graph struct { 
-
-    Nodes [] Node
-    Edges [] Edge
-} 
+type Graph struct {
+	Nodes []Node
+	Edges []Edge
+}
 
 type Node struct {
-    Id string
-    Name string
-    X string
-    Y string
-    Width string
-    Height string
-} 
+	Id     string
+	Name   string
+	X      string
+	Y      string
+	Width  string
+	Height string
+}
 
 type Edge struct {
-    From string
-    To string
-} 
+	From string
+	To   string
+}
 
+func main() {
 
+	pathwayId := flag.String("id", "hsa05200",
+		"The id of the pathway you're interested in")
+	flag.Parse()
 
-func main(){
+	pathway := kegg.NewKeggPathway(*pathwayId)
 
-    pathwayId := flag.String("id", "hsa05200",
-                "The id of the pathway you're interested in")
-    flag.Parse()
+	nodes := make([]Node, len(pathway.Entries))
+	edges := make([]Edge, len(pathway.Relations))
 
-    pathway := kegg.NewKeggPathway(*pathwayId) 
-    
-    nodes := make([]Node, len(pathway.Entries))
-    edges := make([]Edge, len(pathway.Relations))
+	for i, e := range pathway.Entries {
+		info := e.Graphics
+		n := Node{e.Id, info.Name, info.X, info.Y, info.Width, info.Height}
+		nodes[i] = n
+	}
 
-    for i, e := range pathway.Entries {
-        info := e.Graphics
-        n := Node{e.Id, info.Name, info.X, info.Y,info.Width, info.Height}
-        nodes[i] = n
-    }
+	for j, r := range pathway.Relations {
+		e := Edge{r.Entry1, r.Entry2}
+		edges[j] = e
+	}
 
-    for j, r := range pathway.Relations {
-        e := Edge{r.Entry1, r.Entry2} 
-        edges[j] = e
-    } 
+	graph := Graph{nodes, edges}
 
-    
-    graph := Graph{nodes, edges} 
-    
+	b, err := json.Marshal(graph)
 
+	if err != nil {
+		fmt.Println("Could not marshal response ", err)
+		return
+	}
 
-    b, err := json.Marshal(graph)
+	filename := *pathwayId + ".json"
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("could not create json file ", err)
+		return
+	}
+	_, err = file.Write(b)
+	if err != nil {
+		fmt.Println("Could not write to json file ", err)
+	}
 
-    if err != nil {
-        fmt.Println("Could not marshal response ", err)
-        return 
-    }
+	file.Close()
 
-    filename := *pathwayId + ".json"
-    file, err := os.Create(filename)
-    if err != nil {
-        fmt.Println("could not create json file ", err)
-        return 
-    }
-     _, err = file.Write(b)
-    if err != nil{
-        fmt.Println("Could not write to json file ", err)
-    }
-} 
+	// Download and store pathway image
+	url := pathway.Image
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Image could not be downloaded ", err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Could not read response ", err)
+		return
+	}
+
+	filename = *pathwayId + ".png"
+	file, err = os.Create(filename)
+	if err != nil {
+		fmt.Println("Could not create image file ", err)
+	}
+
+	_, err = file.Write(body)
+	if err != nil {
+		fmt.Println("Could not write image ", err)
+		return
+	}
+}
