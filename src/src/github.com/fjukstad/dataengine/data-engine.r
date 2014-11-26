0f00dad1c6fbc25f03242d85edec6463b81e84d1
Bjørn Fjukstad <bjorn@cs.uit.no>
2014-11-26 14:37:20 +0100

    removed old code

--- src/src/github.com/fjukstad/dataengine/data-engine.r
+++ src/src/github.com/fjukstad/dataengine/data-engine.r
@@ -2,8 +2,6 @@ library(xlsx)
 dataset <- read.xlsx("karina/dataset.xlsx", sheetIndex=1)
 options(width=10000) 
 
-#setwd("/Users/bjorn/Dropbox/repos/mine/kvik/src/src/github.com/fjukstad/dataengine")
-
 get <- function(geneName) {
   a = dataset[dataset$Genes == geneName,];
   return (a) 
@@ -17,15 +15,6 @@ fc <- function(genes) {
     a = dataset[match(genes, dataset$Genes),];
     b = as.numeric(as.character(a$dm))
     return (b) 
-
-#  r <- rnorm2(length(genes), 0, 1)
-#  
-#  if(length(genes) == 1) {
-#    r <- rnorm2(2, 0, 1)
-#    return (c(r[1]))
-#  }
-#  
-#  return (c(r))
 }
 
 pvalues <- function(genes) { 
@@ -35,18 +24,5 @@ pvalues <- function(genes) {
 }
 
 exprs <- function(gene) { 
-  #mean = fc(gene)
-  #r <- rnorm2(100,mean,1)
-  #return (c(r))
   return 
-}
-
-rnorm2 <- function(n,mean,sd) { 
-  return (mean+sd*scale(rnorm(n)))
-}
-
-
-
-add <- function(a,b){
-  return (a+b)
 }
