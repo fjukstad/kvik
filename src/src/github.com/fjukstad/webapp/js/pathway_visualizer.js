65dff1e3211c0fc8eb754b3c41a18ae408bd4ceb
Bjørn Fjukstad <bjorn@cs.uit.no>
2014-12-22 10:08:42 +0100

    minor style

--- src/src/github.com/fjukstad/webapp/js/pathway_visualizer.js
+++ src/src/github.com/fjukstad/webapp/js/pathway_visualizer.js
@@ -252,7 +252,7 @@ window.onresize = function(event) {
 
 function resizeViews(){
     var cyt = $('#cy')[0]
-    var h = $(window).height()-60
+    var h = $(window).height()-90
     cyt.style.height = h + "px" 
     cyt.style.width = $(".col-sm-8")[0].clientWidth-25+"px"
 
@@ -274,7 +274,7 @@ function resizeViews(){
     }
 
     var exprs = $("#expression-view")[0]
-    exprs.style.height =  $(window).height()-270+"px"
+    exprs.style.height =  $(window).height()-300+"px"
     exprs.style.width = $(".col-sm-4")[0].clientWidth-30+"px"
     
     try { 
