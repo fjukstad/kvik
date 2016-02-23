ee1918cef13942a13a2ba03ff68e544ceb2b1d38
Bjørn Fjukstad <bjorn@cs.uit.no>
2016-02-23 10:14:21 +0100

    refactor client/server add server func to pkg

--- r/r.go
+++ r/r.go
@@ -1,18 +1,25 @@
-package R
+package r
 
 import (
 	"bytes"
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/rand"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/fjukstad/r"
+	"github.com/gorilla/mux"
 )
 
 var r *rand.Rand
@@ -59,6 +66,7 @@ func installPackages(packages string) error {
 	return nil
 }
 
+// Installs the package found at src. Should be a tgz ball.
 func InstallPackageFromSource(src string) (string, error) {
 
 	cmd := exec.Command("R", "CMD", "INSTALL", src)
@@ -143,6 +151,7 @@ func Call(pkg, fun, args string) (*Session, error) {
 	return &Session{key, wd, out.String(), command}, nil
 }
 
+// get results for a func call
 func Get(key, format string) ([]byte, error) {
 
 	dir := rootDir + "/" + key
@@ -184,5 +193,240 @@ func Get(key, format string) ([]byte, error) {
 	}
 
 	return ioutil.ReadFile(dir + "/output" + extension)
+
+}
+
+type Server struct {
+	Addr     string
+	Username string
+	Server   string
+}
+
+// Remote call.
+func (s *Server) Call(pkg, fun, args string) (string, error) {
+	url := "http://" + s.Addr + "/call"
+
+	c := R.RCall{pkg, fun, args}
+
+	b, err := json.Marshal(c)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	postArgs := strings.NewReader(string(b))
+
+	resp, err := http.Post(url, "application/json", postArgs)
+	if err != nil {
+		return "", err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+// Remote get results
+func (s *Server) Get(key, format string) ([]byte, error) {
+
+	resp, err := http.Get("http://" + s.Addr + "/get/" + key + "/" + format)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return body, nil
+
+}
+
+// Uploads and installs package to remote R Server
+func (s *Server) UploadPackage(src string) error {
+	file, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	fi, err := file.Stat()
+	if err != nil {
+		return err
+	}
+	file.Close()
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", fi.Name())
+	if err != nil {
+		return err
+	}
+	part.Write(fileContents)
+	err = writer.Close()
+	if err != nil {
+		return err
+	}
+
+	url := "http://" + s.Addr + "/install"
+
+	request, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Response:", string(responseBody))
+	return nil
+}
+
+var cache map[string]string
+var logTime = "02-01-2006 15:04:05.000"
+
+func CallHandler(w http.ResponseWriter, r *http.Request) {
+	printTime()
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	call := RCall{}
+	err = json.Unmarshal(body, &call)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Call:", call.Package, call.Function, call.Arguments)
+
+	cacheKey := call.Package + call.Function + call.Arguments
+
+	if cache[cacheKey] != "" {
+		printTime()
+		fmt.Println("CACHE HIT")
+		w.Write([]byte(cache[cacheKey]))
+		return
+	} else {
+		printTime()
+		fmt.Println("CACHE MISS")
+	}
+
+	s, err := Call(call.Package, call.Function, call.Arguments)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if cache == nil {
+		cache = make(map[string]string, 0)
+	}
+
+	cache[cacheKey] = s.Key
+
+	w.Write([]byte(s.Key))
+
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	printTime()
+	vars := mux.Vars(r)
+	key := vars["key"]
+	format := vars["format"]
+
+	fmt.Println("Get: key:", key, "format", format)
+
+	res, err := Get(key, format)
+
+	if err != nil {
+		fmt.Println(err)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write(res)
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Nothing to see here. Try /call, /get/{key}/{format} or /install"))
+}
+
+func InstallHandler(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	defer file.Close()
+
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	fileLoc := "/tmp/file"
+
+	out, err := os.Create(fileLoc)
+	if err != nil {
+		fmt.Fprintf(w, "Failed to open the file for writing")
+		return
+	}
+	defer out.Close()
+	_, err = io.Copy(out, file)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	printTime()
+	fmt.Println("Installing package", header.Filename)
+
+	output, err := InstallPackageFromSource(fileLoc)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", output)
+
+}
+
+func printTime() {
+	fmt.Print(time.Now().Format(logTime), " ")
+}
+
+func StartServer(port, tmpDir string) error {
+	err := Init("/tmp/kvik", "")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", IndexHandler)
+	r.HandleFunc("/call", CallHandler)
+	r.HandleFunc("/get/{key}/{format}", GetHandler)
+	r.HandleFunc("/install", InstallHandler)
+
+	http.Handle("/", r)
+
+	printTime()
+	fmt.Println("Starting server at ", port)
+
+	return http.ListenAndServe(port, r)
 
 }
--- r/tools/client.go
+++ r/tools/client.go
@@ -1,125 +1,29 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"mime/multipart"
-	"net/http"
-	"os"
-	"strings"
 	"time"
 
-	"github.com/fjukstad/r"
+	"github.com/fjukstad/kvik/r"
 )
-
-func UploadPackage(url, src string) error {
-	file, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	fileContents, err := ioutil.ReadAll(file)
-	if err != nil {
-		return err
-	}
-	fi, err := file.Stat()
-	if err != nil {
-		return err
-	}
-	file.Close()
-	body := new(bytes.Buffer)
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("file", fi.Name())
-	if err != nil {
-		return err
-	}
-	part.Write(fileContents)
-	err = writer.Close()
-	if err != nil {
-		return err
-	}
-
-	request, err := http.NewRequest("POST", url, body)
-	if err != nil {
-		return err
-	}
-	request.Header.Add("Content-Type", writer.FormDataContentType())
-
-	client := &http.Client{}
-	resp, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-
-	responseBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Response:", string(responseBody))
-	return nil
-}
-
-func TestFunctionality(server, pkg, fun, args string) (string, error) {
-
-	url := "http://" + server + "/call"
-
-	c := R.RCall{pkg, fun, args}
-
-	b, err := json.Marshal(c)
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-
-	postArgs := strings.NewReader(string(b))
-
-	resp, err := http.Post(url, "application/json", postArgs)
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-
-	resp, err = http.Get("http://" + server + "/get/" + string(body) + "/json")
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-
-	return string(body), nil
-
-}
 
 func main() {
 	t0 := time.Now()
 	var server = flag.String("server", "localhost:8181", "ip:port of server")
 	flag.Parse()
 
-	out, err := TestFunctionality(*server, "stats", "rnorm", "n=10")
-	fmt.Println(out, err)
+	s := r.Server{*server, "", ""}
 
-	url := "http://" + *server + "/install"
-	err = UploadPackage(url, "packages/addman_0.1.tgz")
+	out, err := s.Call("stats", "rnorm", "n=10")
+
+	err = s.UploadPackage("packages/addman_0.1.tgz")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	out, err = TestFunctionality(*server, "addman", "hello", "")
+	out, err = s.Call("addman", "hello", "")
 	fmt.Println(out, err)
 	t1 := time.Now()
 	fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
