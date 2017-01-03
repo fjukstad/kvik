e2802c914bda5bf7bf2acc3e1033f60e98aaca55
Bjørn Fjukstad <bjorn@cs.uit.no>
2017-01-03 17:27:21 -0500

    bugfix example

--- eutils/example/gene.go
+++ eutils/example/gene.go
@@ -4,7 +4,7 @@ import "fmt"
 import "github.com/fjukstad/kvik/eutils"
 
 func main() {
-	ds, err := eutils.GetDocumentSummary("627")
+	ds, err := eutils.GeneSummary("627")
 	if err != nil {
 		fmt.Println(err)
 		return
